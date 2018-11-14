539239e8c7297c2aeeb727fe6f0f6ba29c372001
Serega <ivss.engineering@gmail.com>
2018-11-14 22:58:17 +0200

    New

--- src/Java_course.java
+++ src/Java_course.java
@@ -2,7 +2,7 @@ public class Java_course {
 
     public static void main(String args[]) {
 
-        System.out.println("Hello, World"); //Lesson 1
+        System.out.println("Hello, World");
         System.out.println("Hello");
 
         String b = "b";
