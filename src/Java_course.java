23876bff45d85c40824bd6079c0a9040ca1d6a9a
Serega <ivss.engineering@gmail.com>
2018-11-15 07:38:22 +0200

    Array 10x10

--- src/Java_course.java
+++ src/Java_course.java
@@ -1,16 +1,20 @@
+import java.util.Random;
+
 public class Java_course {
 
     public static void main(String args[]) {
 
-        System.out.println("Hello, World");
-        System.out.println("Hello");
+        int[][] mas2d = new int[10][10];
+        Random rand = new Random();
 
-        String b = "b";
-        String c = "c";
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+                mas2d[i][j] = rand.nextInt(100);
 
-        byte a = 12;
-        a = (byte) (a + 1);
-
-        System.out.println(a);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++)
+                System.out.print(mas2d[i][j] + " ");
+            System.out.println();
+        }
     }
 }
